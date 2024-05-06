62b5ad8a85abee1365987b3f962f79d98cc1071d
Vincent Alfarieco Putra Damanik <126937409+VincentAlfa@users.noreply.github.com>
2024-05-06 21:13:45 +0700

    Time: 27 ms (43.78%), Space: 6.9 MB (52.04%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		m := (l + r) / 2
+
+		if target > nums[m] {
+			l = m + 1
+		} else if target < nums[m] {
+			r = m - 1
+		} else {
+			return m
+		}
+	}
+	return -1
+}
